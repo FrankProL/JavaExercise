0215a509917dd890e177028229458886bd75595a
Frank <ljffrank@163.com>
2018-10-27 17:57:21 +0800

    design atterns init

--- Algorithm/src/main/java/recursion/perm.java
+++ Algorithm/src/main/java/recursion/perm.java
@@ -12,13 +12,14 @@ public class perm {
         list[i] = t;
     }
 /*
-递归另一种写法：或者采用July的方法：
+递归另一种写法：July方法：
 
  从集合中依次选出每一个元素，作为排列的第一个元素，然后对剩余的元素进行全排列，如此递归处理，
 * 从而得到所有元素的全排列。以对字符串abc进行全排列为例，我们可以这么做：以abc为例：
 * 固定a，求后面bc的排列：abc，acb，求好后，a和b交换，得到bac
 * 固定b，求后面ac的排列：bac，bca，求好后，c放到第一位置，得到cba
 * 固定c，求后面ba的排列：cba，cab。
+* k是固定的元素，m是元素个数
  */
     public static void perm(Object[] list, int k, int m) {
         if (k == m) {
@@ -37,6 +38,10 @@ public class perm {
 
     public static void permutation1(String str, String result, int len) {
         /* 全排列 递归实现
+        递归思路：遍历，从第一个字母开始构建一个排列，添加第一字母，添加第二个字母，添加第三个字母
+                result ""-->""+str.charAt(i)-->""+str.charAt(i)+str.charAt(j)
+                每次添加的都是result中不存在的字符，直到result长度与原字符串长度一样
+                （字符串中不能有重复字符）
       递归树：
           str:          a            b                c
                       ab ac         ba   bc         ca   cb
@@ -47,7 +52,7 @@ public class perm {
             System.out.println(result);
         } else {
             for (int i = 0; i < str.length(); i++) {
-                if (result.indexOf(str.charAt(i)) < 0) {    //返回指定字符在此字符串中第一次出现处的索引。
+                if (result.indexOf(str.charAt(i)) < 0) {    //indexOf返回指定字符在此字符串中第一次出现处的索引。
 //                    System.out.println("字母："+str.charAt(i));
                     permutation1(str, result + str.charAt(i), len);
                 }
@@ -56,11 +61,11 @@ public class perm {
     }
 
     public static void main(String[] args) {
-//        Object[] list = new Object[5];
-//        for (int i = 0; i < list.length; i++) {
-//            list[i] = "sasdf" + i;
-//        }
-//        perm(list, 0, 4);
+        Object[] list = new Object[5];
+        for (int i = 0; i < list.length; i++) {
+            list[i] = "sasdf" + i;
+        }
+        perm(list, 0, 4);
 
 
         String s = "abc";
